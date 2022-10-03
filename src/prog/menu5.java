59518bb955a076f55de53d08801ef1d4a532e932
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2022-10-03 22:52:56 +0700

    Update menu5.java

--- src/prog/menu5.java
+++ src/prog/menu5.java
@@ -26,15 +26,18 @@ public class menu5 {
     public static void BicubicTerm(Matriks M){
         Matriks a = Bicubic.bicubic(M);
 
-        System.out.print("Input nilai x,y yang ingin dicari (mis: 0.5 0.5): ");
+        System.out.println("Input nilai x,y yang ingin dicari (mis: 0.5 0.5): ");
 
         Scanner inputxy = new Scanner(System.in);
-        String [] xy = inputxy.nextLine().split("\\s+");
+
+        String[] xy = inputxy.nextLine().split("\\s+");
         int[] numxy = new int[2];
-        for (int i = 0; i < 2; i++){
+
+        for(int i = 0; i < 2; i++){
             numxy[i] = Integer.parseInt(xy[i]);
         }
         inputxy.close();
+
 
         // hasil
         float hasil = Bicubic.hasilbic(a,numxy[0],numxy[1]);
