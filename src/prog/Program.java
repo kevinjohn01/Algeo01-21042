d2ee808e28a2577c547df0a2edc844d30f929b17
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2022-10-03 16:25:28 +0700

    yeyyy bisa pililh menu

--- src/prog/Program.java
+++ src/prog/Program.java
@@ -14,90 +14,90 @@ public class Program{
             System.out.println("MENU\n1. Sistem Persamaan Linier\n2. Determinan\n3. Matriks Balikan\n4. Interpolasi Polinom\n5. Interpolasi Bicubic\n6. Regresi Linier Berganda\n7. Keluar");
             
             // input menu
-            System.out.println("Masukkan pilihan menu: ");
+            System.out.print("Masukkan pilihan menu: ");
             
-            String inp = input_menu.nextLine();
-            while (!(inp == "1" || inp == "2" || inp == "3" || inp == "4" || inp == "5" || inp == "6" || inp == "7")){
-                System.out.println("Masukkan pilihan menu: ");
-                inp = input_menu.nextLine();
+            int inp = Integer.parseInt(input_menu.nextLine());
+            while (!(inp == 1 || inp == 2 || inp == 3 || inp == 4 || inp == 5 || inp == 6 || inp == 7)){
+                System.out.print("Masukkan pilihan menu: ");
+                inp = Integer.parseInt(input_menu.nextLine());
             }
 
-            if (inp == "1"){ // 1. Sistem Persamaan Linier
+            if (inp == 1){ // 1. Sistem Persamaan Linier
                 // pilih submenu
                 System.out.println("1. Metode Eliminasi Gauss\n2. Metode Eliminasi Gauss-Jordan\n3. Metode matriks balikan\n4. Kaidah Cramer");
-                inp = input_menu.nextLine();
-                while(!(inp == "1" || inp == "2" || inp == "3" || inp == "4")){
-                    System.out.println("Masukkan pilihan menu: ");
-                    inp = input_menu.nextLine();
+                inp = Integer.parseInt(input_menu.nextLine());
+                while(!(inp == 1 || inp == 2 || inp == 3 || inp == 4)){
+                    System.out.print("Masukkan pilihan menu: ");
+                    inp = Integer.parseInt(input_menu.nextLine());
                 }
 
-                if (inp == "1"){ // 1. Metode Eliminasi Gauss
-
-                }
-
-                else if (inp == "2"){ // 2. Metode Eliminasi Gauss-Jordan
+                if (inp == 1){ // 1. Metode Eliminasi Gauss
 
                 }
 
-                else if (inp == "3"){ // 3. Metode matriks balikan
+                else if (inp == 2){ // 2. Metode Eliminasi Gauss-Jordan
+
+                }
+
+                else if (inp == 3){ // 3. Metode matriks balikan
                     
 
                 }
                 
-                else if (inp == "4"){ // 4. Kaidah Cramer
+                else if (inp == 4){ // 4. Kaidah Cramer
 
                 }
 
             }
 
-            else if (inp == "2"){ // 2. Determinan
+            else if (inp == 2){ // 2. Determinan
                 // pilih submenu
                 System.out.println("1. Metode reduksi baris\n2. Metode ekspansi kofaktor");
-                inp = input_menu.nextLine();
-                while(!(inp == "1" || inp == "2")){
-                    System.out.println("Masukkan pilihan menu: ");
-                    inp = input_menu.nextLine();
+                inp = Integer.parseInt(input_menu.nextLine());
+                while(!(inp == 1 || inp == 2)){
+                    System.out.print("Masukkan pilihan menu: ");
+                    inp = Integer.parseInt(input_menu.nextLine());
                 }
 
-                if (inp == "1"){ // 1. Metode reduksi baris
+                if (inp == 1){ // 1. Metode reduksi baris
 
                 }
 
-                else if (inp == "2"){ // 2. Metode Ekspansi Kofaktor
+                else if (inp == 2){ // 2. Metode Ekspansi Kofaktor
 
                 }
             }
 
-            else if (inp == "3"){ // 3. Matriks Balikan
+            else if (inp == 3){ // 3. Matriks Balikan
                 // pilih submenu
                 System.out.println("1. Metode reduksi baris\n2. Metode adjoin");
-                inp = input_menu.nextLine();
-                while(!(inp == "1" || inp == "2")){
-                    System.out.println("Masukkan pilihan menu: ");
-                    inp = input_menu.nextLine();
+                inp = Integer.parseInt(input_menu.nextLine());
+                while(!(inp == 1 || inp == 2)){
+                    System.out.print("Masukkan pilihan menu: ");
+                    inp = Integer.parseInt(input_menu.nextLine());
                 }
 
-                if (inp == "1"){ // 1. Metode reduksi baris
+                if (inp == 1){ // 1. Metode reduksi baris
 
                 }
-                else if (inp == "2"){ // 2. Metode adjoin
+                else if (inp == 2){ // 2. Metode adjoin
 
                 }
             }
 
-            else if (inp == "4"){ // 4. Interpolasi Polinom
+            else if (inp == 4){ // 4. Interpolasi Polinom
 
             }
 
-            else if (inp == "5"){ // 5. Interpolasi Bicubic
+            else if (inp == 5){ // 5. Interpolasi Bicubic
 
             }
 
-            else if (inp == "6"){ // 6. Regresi Linier Berganda
+            else if (inp == 6){ // 6. Regresi Linier Berganda
 
             }
 
-            else if (inp == "7"){ // 7. Keluar
+            else if (inp == 7){ // 7. Keluar
                 exit = true;
             }
 
