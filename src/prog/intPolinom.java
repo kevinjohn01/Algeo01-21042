e6af4dafa0c30295840371813698c741595cc6d2
kevinjohn01 <16521168@std.stei.itb.ac.id>
2022-10-03 22:43:29 +0700

    huh

--- src/prog/intPolinom.java
+++ src/prog/intPolinom.java
@@ -4,6 +4,7 @@ import matriks.*;
 
 import java.util.Scanner;
 
+
 import matriks.InterpolasiPolinom;
 import matriks.spl.SolusiSPL;
 
@@ -14,11 +15,13 @@ import matriks.spl.SolusiSPL;
 public class intPolinom {
     public static void tabel(){
         Scanner input = new Scanner(System.in);
-
+        float[][] MI = new float[][] {{0.4f,0.043f},{0.7f,0.005f},{0.11f,0.058f},{0.14f,0.072f},{0.17f,0.1f},{0.2f,0.13f},{0.23f,0.147f}};
+        //float[][] MI = new float[][] {{1,1},{2,2},{3,3},{4,4}};
+        Matriks M = Matriks.from(MI);
         System.out.print("x = ");
         String m = input.nextLine();
         float n = Float.parseFloat(m);
-        float y = InterpolasiPolinom.nilaifungsi(InterpolasiPolinom.interpolasipolinom(), n);
+        float y = InterpolasiPolinom.nilaifungsi(InterpolasiPolinom.interpolasipolinom(M), n);
         System.out.print("f(x) = ");
         System.out.print(y);
         input.close();
@@ -40,8 +43,11 @@ public class intPolinom {
         else {
             hari = 30;
         }
-        float tanggal = mm + (dd /(hari));
-        SolusiSPL k = InterpolasiPolinom.interpolasipolinom();
+        float tanggal = mm + ((float) dd /(hari));
+        //System.out.println(tanggal);
+        float[][] MI = new float[][] {{6.567f,12624},{7f,21807},{7.258f,38391},{7.451f,54517},{7.548f,51952},{7.839f,28228},{8.161f,35764},{8.484f,20813},{8.709f,12408},{9f,10534}};
+        Matriks M = Matriks.from(MI);
+        SolusiSPL k = InterpolasiPolinom.interpolasipolinom(M);
         float hasil = InterpolasiPolinom.nilaifungsi(k,tanggal);
         System.out.print(hasil);
         input.close();
@@ -58,7 +64,7 @@ public class intPolinom {
         Matriks MI = new Matriks(n,2);
         float dx = 2/n;
         float curr_x = 0;
-        for ( int i = 0; i<=n; i++){
+        for ( int i = 0; i<n; i++){
             curr_elmt = function(curr_x);
             MI.set(i,1,curr_elmt);
             MI.set(i,0,curr_x);
@@ -70,9 +76,15 @@ public class intPolinom {
     }
 
     public static float function(float x) {
-        double hasil;
+        double hasil, pembilang, penyebut;
         double e = 2.71828182845904523536028747135266249;
-        hasil = (x*x + Math.pow(x,0.5))/(Math.pow(e,x) + x);
-        return (float) hasil; 
+        pembilang =((double)x*x) + Math.pow(x,0.5);
+        penyebut = Math.pow(e,x) + ((double) x);
+        //System.out.println(pembilang);
+        //System.out.println(penyebut);
+        hasil = (pembilang)/(penyebut);
+        float hasilf = (float) hasil;
+        //System.out.println(hasil);
+        return (float) hasilf; 
     }
 }
